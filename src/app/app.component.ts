f5eae9c39ff912e49f19248cb2d0c4815ac17920
Sahil Makhijani <svmakhijani94@gmail.com>
2021-11-29 01:55:04 -0700

    Added dark mode toggle functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from './core/services/dark-mode.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  darkMode = false;
+
+  constructor(private darkModeService: DarkModeService) {
+    this.darkModeService.isDarkOn.subscribe((bool) => {
+      this.darkMode = bool;
+    });
+  }
 }
