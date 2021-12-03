38f44bfc2ce5fd97cad1111f15dd95c93a8de435
Sahil Makhijani <svmakhijani94@gmail.com>
2021-12-03 15:24:31 -0700

    Added & configured home module routing

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import { HomeComponent } from './home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
