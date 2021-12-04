2668ef52eb0f4ced218192f3eff2ebcd52b653cc
Sahil Makhijani <svmakhijani94@gmail.com>
2021-12-03 22:42:09 -0700

    Set up Firestore data retrieval

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Home } from '../core/models/home';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  default: Home | undefined;
+  promo: Home | undefined;
+
+  constructor(private store: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.store.collection('home').doc<Home>('promo').valueChanges().subscribe(res => {
+      if (res && this.isHomeType(res)) {
+        this.promo = res;
+        console.log(this.promo);
+      }
+    });
+    this.store.collection('home').doc<Home>('default').valueChanges().subscribe(res => {
+      if (res && this.isHomeType(res)) {
+        this.default = res;
+        console.log(this.default);
+      }
+    });
+  }
+
+  isHomeType(obj: any): obj is Home {
+    return obj.title !== undefined;
   }
 
 }
