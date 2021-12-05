e9fce2a4c50efcdd159f4af17fb780d529a66e7c
Sahil Makhijani <svmakhijani94@gmail.com>
2021-12-04 17:17:18 -0700

    Removed unused code from firestore service

--- src/app/core/services/firestore.service.ts
+++ src/app/core/services/firestore.service.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@angular/core';
 import { doc, docData, Firestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { Home } from '../models/home';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
-
-  default: Observable<Home | undefined> = new Observable;
-  promo: Observable<Home | undefined> = new Observable;
 
   constructor(private firestore: Firestore) { }
 
