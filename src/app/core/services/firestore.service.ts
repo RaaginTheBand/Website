c93b3c0bfd266e1dbbb67d7eb45fbab75b9789c0
Sahil Makhijani <svmakhijani94@gmail.com>
2021-12-04 20:40:24 -0700

    Refactored method in Firestore service

--- src/app/core/services/firestore.service.ts
+++ src/app/core/services/firestore.service.ts
@@ -10,6 +10,6 @@ export class FirestoreService {
   constructor(private firestore: Firestore) { }
 
   getData<T>(collectionName: string, documentName: string): Observable<T> {
-    return docData(doc(this.firestore, `${collectionName}/${documentName}`)) as Observable<T>;
+    return docData(doc(this.firestore, collectionName, documentName)) as Observable<T>;
   }
 }
