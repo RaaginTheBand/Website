a8b4247a4685f1dac8e7dbd8e95bd3082d2eaafe
Sahil Makhijani <svmakhijani94@gmail.com>
2021-12-06 01:13:30 -0700

    Added contact component to module routing

--- src/app/contact/contact-routing.module.ts
+++ src/app/contact/contact-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ContactComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
