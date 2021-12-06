894bc49eae1b0de6222a07cb98dfb3cb4cd9d901
Sahil Makhijani <svmakhijani94@gmail.com>
2021-12-06 00:41:51 -0700

    Added route for about module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
   { path: '**', redirectTo: '/' }
 ];
 
