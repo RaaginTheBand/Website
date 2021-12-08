1cdd593b256c130d0ca61f61feb339d65bda00e0
Sahil Makhijani <svmakhijani94@gmail.com>
2021-12-08 01:16:18 -0700

    Added loading spinner & unsubscriptions

--- src/app/about/members/members.component.ts
+++ src/app/about/members/members.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { Members } from '../../core/models/about';
+import { DarkModeService } from '../../core/services/dark-mode.service';
 import { FirestoreService } from '../../core/services/firestore.service';
 
 @Component({
@@ -8,22 +11,31 @@ import { FirestoreService } from '../../core/services/firestore.service';
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.scss']
 })
-export class MembersComponent implements OnInit {
+export class MembersComponent implements OnInit, OnDestroy {
 
+  color = '';
   content: Members = {} as Members;
   isLoaded = false;
   names: string[] = [];
+  private unsubscribe: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  constructor(private firestoreService: FirestoreService) { }
+  constructor(private darkmodeService: DarkModeService,
+              private firestoreService: FirestoreService) { }
 
   ngOnInit(): void {
-    this.firestoreService.getData<Members>('about', 'members').subscribe(res => {
+    this.darkmodeService.isDarkOn.pipe(takeUntil(this.unsubscribe)).subscribe(res => this.color = (res) ? this.darkmodeService.spinnerDark : this.darkmodeService.spinnerLight);
+    this.firestoreService.getData<Members>('about', 'members').pipe(takeUntil(this.unsubscribe)).subscribe(res => {
       if (res) {
         this.content = res;
         this.names = Object.keys(res).sort();
         this.isLoaded = true;
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next(true);
+    this.unsubscribe.unsubscribe();
   }
 
 }
