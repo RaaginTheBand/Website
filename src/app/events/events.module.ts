df5ccd62ed6a4f83369f3f54c83827bcf8da6773
Sahil Makhijani <svmakhijani94@gmail.com>
2021-12-09 22:25:36 -0700

    Created past events component

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { EventsRoutingModule } from './events-routing.module';
 import { EventsComponent } from './events.component';
 import { UpcomingComponent } from './upcoming/upcoming.component';
+import { PastComponent } from './past/past.component';
 
 
 @NgModule({
   declarations: [
     EventsComponent,
-    UpcomingComponent
+    UpcomingComponent,
+    PastComponent
   ],
   imports: [
     CommonModule,
