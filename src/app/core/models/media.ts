9863a4aee36df7c7980af12184dd4ef0653997b2
Sahil Makhijani <svmakhijani94@gmail.com>
2022-01-30 00:22:27 -0700

    Changed media section to reflect data changes

--- src/app/core/models/media.ts
+++ src/app/core/models/media.ts
@@ -1,10 +1,20 @@
 export interface Photo {
+  photos: PhotoInfo[];
+  title: string;
+}
+
+export interface Video {
+  videos: VideoInfo[];
+  title: string;
+}
+
+interface PhotoInfo {
   alt: string;
   caption: string;
   url: string;
 }
 
-export interface Video {
+interface VideoInfo {
   caption: string;
   type: string;
   url: string;
