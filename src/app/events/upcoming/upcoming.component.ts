3a38c59b4f995448d3d02047f737d3584d92b5c7
Sahil Makhijani <svmakhijani94@gmail.com>
2022-01-29 20:19:03 -0700

    Added text for no upcoming events

--- src/app/events/upcoming/upcoming.component.ts
+++ src/app/events/upcoming/upcoming.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Upcoming } from '../../core/models/events';
 
@@ -7,12 +7,19 @@ import { Upcoming } from '../../core/models/events';
   templateUrl: './upcoming.component.html',
   styleUrls: ['./upcoming.component.scss']
 })
-export class UpcomingComponent {
+export class UpcomingComponent implements OnInit {
 
   @Input('spinnerColor') color = '';
   @Input('data') content: Upcoming = {} as Upcoming;
   @Input() isLoaded = false;
+  isEvent = true;
 
   constructor() { }
+
+  ngOnInit(): void {
+    if (Object.keys(this.content).length == 0) {
+      this.isEvent = false;
+    }
+  }
 
 }
