15821400b1e7d0b46a2273b2894cffc8bd9cc261
Sahil Makhijani <svmakhijani94@gmail.com>
2022-01-30 20:09:33 -0700

    Installed nodemailer for contact form

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,20 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { DarkModeService } from './services/dark-mode.service';
+import { EmailService } from './services/email.service';
 import { FirestoreService } from './services/firestore.service';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   providers: [
     DarkModeService,
+    EmailService,
     FirestoreService
   ]
 })
