63566779ecc5edd055178d48e745f846592336af
Sahil Makhijani <svmakhijani94@gmail.com>
2022-02-20 20:48:20 -0700

    Updated history section styling for desktop & mobile

--- src/app/about/history/history.component.ts
+++ src/app/about/history/history.component.ts
@@ -1,18 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { History } from '../../core/models/about';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
 
   @Input('spinnerColor') color = '';
   @Input('data') content: History = {} as History;
   @Input() isLoaded = false;
+  isPortrait = false;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe(['(orientation: portrait)']).subscribe((state: BreakpointState) => {
+      if (state) {
+        this.isPortrait = state.matches;
+      }
+    });
+  }
 
 }
